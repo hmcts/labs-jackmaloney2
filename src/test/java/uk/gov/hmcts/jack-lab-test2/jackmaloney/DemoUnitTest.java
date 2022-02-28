899919166200b1c3182684f2924b894a0cc6715e
Scaffolder <scaffolder@backstage.io>
2022-02-28 14:12:08 +0000

    Initial commit

---
+++ src/test/java/uk/gov/hmcts/jack-lab-test2/jackmaloney/DemoUnitTest.java
@@ -0,0 +1,14 @@
+package uk.gov.hmcts.jack-lab-test2.jackmaloney;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+// TODO Delete this when you create your first test
+public class DemoUnitTest {
+
+    @Test
+    public void exampleOfTest() {
+        assertTrue(System.currentTimeMillis() > 0, "Example of Unit Test");
+    }
+}
