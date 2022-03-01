1b566f06d1cec696d17305ef33fd8916c340bba8
jackmaloney <jack.maloney@transformuk.com>
2022-03-01 10:50:07 +0000

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/reform/controllers/RootController.java
@@ -1,15 +1,15 @@
 package uk.gov.hmcts.reform.controllers;
 
-import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
-import static org.springframework.http.ResponseEntity.ok;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.TEXT_PLAIN_VALUE;
+import static org.springframework.http.ResponseEntity.ok;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * Default endpoints per application.
